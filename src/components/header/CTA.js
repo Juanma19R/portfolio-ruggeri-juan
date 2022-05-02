e718eabec0048f385a4fcb792bb50ad2ed5e7108
Juanma19R <juan.ruggeri@hotmail.com>
2022-05-01 23:11:00 -0300

    Desarrollo y diseño de la section Header

---
+++ src/components/header/CTA.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CV from '../../assets/CV-Ruggeri-Juan.pdf';
+
+const CTA = () => {
+    return (
+        <div className='cta'>
+            <a href={CV} className='btn' download>Descargar CV</a>
+            <a href='#contact' className='btn btn-primary'>Hablemos</a>
+        </div>
+    )
+}
+
+export default CTA;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import './header.css';
+import CTA from './CTA';
+import ME from '../../assets/me.png';
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <header>
+            <div className='container header__container'>
+                <h5>Hola soy</h5>
+                <h1>Juan Ruggeri</h1>
+                <h5 className='text-light'>Desarrollador Front-end</h5> 
+                <CTA /> 
+                <HeaderSocials />
+
+                <div className='me'>
+                    <img src={ME} alt='Foto de Juan Ruggeri'/>
+                </div>
+
+                <a href='#contact' className='scroll__down'>Scroll down</a>
+            </div>
+        </header>
     )
 }
 
