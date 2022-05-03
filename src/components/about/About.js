f2c5d24d8234097f83dd98aeb7390f9f9d9c6190
Juanma19R <juan.ruggeri@hotmail.com>
2022-05-02 22:43:57 -0300

    Desarrollo de la section about, se añadieron estilos y queries

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,9 +1,49 @@
 import React from 'react';
 import './about.css';
+import ME from '../../assets/me-about.jpg';
+import {FaAward} from 'react-icons/fa';
+import {FiUsers} from 'react-icons/fi';
+import {VscFolderLibrary} from 'react-icons/vsc';
 
 const About = () => {
     return (
-        <section id='about'>About</section>
+        <section id='about'>
+            <h5>Conóceme</h5>
+            <h2>Sobre mí</h2>
+
+            <div className='container about__container'>
+                <div className='about__me'>
+                    <div className='about__me-image'>
+                        <img src={ME} alt='Foto de Ruggeri Juan' />
+                    </div>
+                </div>
+
+                <div className='about__content'>
+                    <div className='about__cards'>
+                        <article className='about__card'>
+                            <FaAward className='about__icon'/>
+                            <h5>Experiencia</h5>
+                            <small>1+ año</small>
+                        </article>
+
+                        <article className='about__card'>
+                            <FiUsers className='about__icon'/>
+                            <h5>Clientes</h5>
+                            <small>100+</small>
+                        </article>
+
+                        <article className='about__card'>
+                            <VscFolderLibrary className='about__icon'/>
+                            <h5>Proyectos</h5>
+                            <small>30+</small>
+                        </article>
+                    </div>
+                        <p>Proactivo, responsable y resolutivo, soy un desarrollador motivado por aprender tecnologías que me ayuden a crecer en el área profesional. Actualmente estoy culminando la carrera de Desarrollo Front-end React en Coder House y participando del programa ONE - Oracle Next Education de Alura Latam y Oracle.</p>
+
+                        <a href='#contact' className='btn btn-primary'>Hablemos!</a>
+                </div>
+            </div>
+        </section>
     )
 }
 
--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -5,7 +5,7 @@ const CTA = () => {
     return (
         <div className='cta'>
             <a href={CV} className='btn' download>Descargar CV</a>
-            <a href='#contact' className='btn btn-primary'>Hablemos</a>
+            <a href='#contact' className='btn btn-primary'>Hablemos!</a>
         </div>
     )
 }
