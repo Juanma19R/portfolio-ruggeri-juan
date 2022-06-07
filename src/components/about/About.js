d43590e976f0c50b9bf838f9dcd5bf8247415e5d
Juanma19R <juan.ruggeri@hotmail.com>
2022-06-07 19:20:13 -0300

    Se agrego a la seccion portfolio, el segundo sprint de ONE

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -29,7 +29,7 @@ const About = () => {
                         <article className='about__card'>
                             <VscFolderLibrary className='about__icon'/>
                             <h5>Proyectos completados</h5>
-                            <small>8</small>
+                            <small>9</small>
                         </article>
 
                         <article className='about__card'>
--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -7,6 +7,7 @@ import IMG4 from '../../assets/portfolio4.jpg';
 import IMG5 from '../../assets/portfolio5.jpg';
 import IMG6 from '../../assets/portfolio6.jpg';
 import IMG7 from '../../assets/portfolio7.jpg';
+import IMG8 from '../../assets/portfolio8.jpg';
 
 const data = [
     {
@@ -64,6 +65,14 @@ const data = [
         desc: 'Proyecto creado con la API de OpenWeatherMap, ReactJS y maquetado con Bootstrap 5.',
         github: 'https://github.com/Juanma19R/Practica-01',
         demo: 'https://github.com/Juanma19R/Practica-01'
+    },
+    {
+        id: 8,
+        image: IMG8,
+        title: 'Juego del ahorcado',
+        desc: 'Segundo SPRINT del programa ONE (Oracle Next Education), fue construido con html5, css3 y javascript. Consta de adivinar la palabra secreta teniendo 10 oportunidades. Tambien cuenta con la opción de agregar tus propias palabras teniendo un limite de 8 letras y letras en mayúscula.',
+        github: 'https://github.com/Juanma19R/Sprint-02-alura',
+        demo: 'https://juanma19r.github.io/Sprint-02-alura/'
     }
 ];
 
